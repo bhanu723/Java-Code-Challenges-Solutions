be35ddc5e6df201c0b6d35209bb2b8e685a0c4b7
bhanu <bhanu@192.168.0.39>
2022-06-03 00:09:55 -0400

    added additional comments for better understanding of Palindome number

--- JavaChallenges/src/leetcode/PalindromNumber.java
+++ JavaChallenges/src/leetcode/PalindromNumber.java
@@ -8,19 +8,26 @@ public class PalindromNumber {
 
 	public static void main(String[] args) {
 
-		int n = 121;
-		System.out.println(isPalindrome(n));
+//		int n = 121;
+		
+		System.out.println(isPalindrome(12121));
+		System.out.println(isPalindrome(-121)); 
+		System.out.println(isPalindrome(999)); 
+		System.out.println(isPalindrome(87)); 
+		System.out.println(isPalindrome(9)); 
+		
 	}
-
 	public static boolean isPalindrome(int x) {
 
 		if (x < 0 || (x != 0 && x % 10 == 0))
 			return false;
-		int rev = 0;
-		while (x > rev) {
-			rev = rev * 10 + x % 10;
+		int reverse = 0;
+		while (x > reverse) { // loop until half, no need to go through all digits
+			reverse = reverse * 10 + x % 10;
 			x = x / 10;
+//			System.out.println("x="+x);
+//			System.out.println("reverse ="+reverse);
 		}
-		return (x == rev || x == rev / 10);
+		return (x == reverse || x == reverse / 10);
 	}
 }
