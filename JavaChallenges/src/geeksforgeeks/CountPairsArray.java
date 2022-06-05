848ef700d211922fa680d1442b2b5285d7f857c7
bhanu <bhanu@192.168.0.39>
2022-06-05 11:18:51 -0400

    Added additional concepts

--- JavaChallenges/src/geeksforgeeks/CountPairsArray.java
+++ JavaChallenges/src/geeksforgeeks/CountPairsArray.java
@@ -4,32 +4,38 @@ package geeksforgeeks;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * @Author:BhanuSurendra Deepala
+ */
 public class CountPairsArray {
 
 	public static void main(String[] args) {
-		
-		int[] arr= {1, 1, 1, 1};
-		int n=arr.length;
-		int k=2;
-		System.out.println(getPairsCount(arr,n,k));
+
+		int[] arr = { 1, 1, 1, 1 };
+		int n = arr.length;
+		int k = 2;
+		System.out.println(getPairsCount(arr, n, k));
 	}
-	
-	 public static int getPairsCount(int[] arr, int n, int k) {
-		 
-			/*
-			 * int counter=0; for(int i=0;i<arr.length;i++) { for(int
-			 * j=i+1;j<arr.length;j++) { if(arr[i]+arr[j]==k) counter++; } } return counter;
-			 */
-		 
-		       Map<Integer,Integer> map = new HashMap<Integer,Integer> ();
-		       int count = 0;
-		       for(int i=0;i<n;++i)
-		       {
-		           if(map.containsKey(k-arr[i]))
-		               count+=map.get(k-arr[i]);
-		           map.put(arr[i],map.getOrDefault(arr[i],0)+1);
-		       }
-		       return count;
-	    }
+
+	public static int getPairsCount(int[] arr, int n, int k) {
+
+		
+		/*
+		 * Approach 1: Brute force solution
+		 * 
+		 * int counter=0; for(int i=0;i<arr.length;i++) { for(int
+		 * j=i+1;j<arr.length;j++) { if(arr[i]+arr[j]==k) counter++; } } return counter;
+		 */
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int count = 0;
+		for (int i = 0; i < n; ++i) {
+			if (map.containsKey(k - arr[i]))
+				count += map.get(k - arr[i]);
+			System.out.println(count);
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
+		return count;
+	}
 
 }
--- JavaChallenges/src/javaimportantconcepts/EmptyEmailException.java
+++ JavaChallenges/src/javaimportantconcepts/EmptyEmailException.java
@@ -1,7 +1,5 @@
 package javaimportantconcepts;
 
-import java.util.Arrays;
-
 public class EmptyEmailException extends Throwable{
 
 	public EmptyEmailException(String message){
